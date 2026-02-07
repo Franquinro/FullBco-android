56fc1e89b44343bf8d8c0c00027585e633a6877e
Fran <fran.quinro@gmail.com>
2026-02-07 07:14:17 +0000

    backs no cierra

--- app/src/main/java/org/franquin/fullbco/FullscreenActivity.java
+++ app/src/main/java/org/franquin/fullbco/FullscreenActivity.java
@@ -335,7 +335,6 @@ public class FullscreenActivity extends FragmentActivity {
     // ----------------------------------------------------------------------------
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
         if (vistaweb.canGoBack()) {
             vistaweb.goBack();
         } else {
